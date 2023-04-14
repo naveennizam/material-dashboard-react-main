9a8eebdd945a1b88a7abd3520ceb4cfa106cb1dc
Naveen Nizam <naveennizam01@gmail.com>
2023-04-14 12:53:30 +0500

    done

--- routes/authRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import OrderItem from '../pages/api/admin/view-orders'
-const router = express.Router();
-
-
-router.get("/view-order", OrderItem);
-
-
-module.exports = router;
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors())
 
 connection.connect()
 let tableName = 'order_items';
-let order_table = 'order_transactions'
+// let order_table = 'order_transactions'
 let currDate = new Date().toJSON().slice(0, 10);
 var today = new Date()
 var prevDate = new Date(new Date().setDate(today.getDate() - 30)).toJSON().slice(0, 10);
